154578e4d68dadb93e3d2530c261e154be2aeaab
yooo-mean <furo9899@naver.com>
2023-04-04 13:03:55 +0900

    feat: 메인 함수 매개변수 받기

--- HelloCSharp/HelloCSharp/Program.cs
+++ HelloCSharp/HelloCSharp/Program.cs
@@ -10,7 +10,22 @@ namespace HelloCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! CSharp!");
+            //매개변수가 없을 경우
+            if (args.Length < 1) Console.WriteLine("Hello! CSharp!");
+            //매개변수가 있을 경우
+            else
+            {
+                Console.Write("Hello! ");
+                //매개변수를 모두 나열
+                for(int i = 0; i < args.Length; i++) {
+                    if (i != args.Length - 1) Console.Write(args[i] + ", ");
+                    //마지막 매개변수는 !와 함께 마무리
+                    else Console.WriteLine(args[i] + "!");
+                }
+            }
+            Console.WriteLine("매개변수로 전달 된 문자열 수 : " + args.Length);
+            Console.Write("아무 키나 누를 시 종료");
+            Console.ReadKey();
         }
     }
 }
